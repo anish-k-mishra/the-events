e5122b6613179cb69d3c055224ad8a96723d5dfb
Anish Mishra <anishkumarc92@gmail.com>
2024-09-13 13:22:19 +0530

    Project initiated and clerk authentication done

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import {Poppins} from "next/font/google";
+import { ClerkProvider,SignInButton, SignedIn, SignedOut,UserButton } from "@clerk/nextjs";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+});
+// const geistMono = Poppins({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Best platform for event management",
+  icons: {
+    icon:'/assets/images/logo.svg' 
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={poppins.variable}
+        >
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ components/shared/Header.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='w-full border-b '>
+        <div className='wrapper'>
+            <Link href='/' className='w-36'>
+                <Image src='/assets/images/logo.svg' width={128} height={38} alt='logo'/>
+            </Link>
+
+            <div className='flex w-32 justify-end gap-3'> 
+
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
