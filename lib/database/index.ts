27fcc4b55ae8049e7f45650f04e22bda3bf68091
Anish Mishra <anishkumarc92@gmail.com>
2024-09-22 12:00:48 +0530

    MongoDB connection and schema done

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -6,4 +6,14 @@ let cached = (global as any).mongoose || {conn: null, promise: null};
 
 export const connectToDatabase = async()=>{
     if(cached.conn) return cached.conn;
+
+    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+        dbName: 'The Events',
+        bufferCommands: false,
+    })
+
+    cached.conn = await cached.promise;
+    return cached.conn;
 }
