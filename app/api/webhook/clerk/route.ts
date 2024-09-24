29b9f32497ca96271ee982fc7ffaa653811ab73d
Anish Mishra <anishkumarc92@gmail.com>
2024-09-24 13:04:35 +0530

    Sync of clerk with backend done

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,6 +1,8 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -57,14 +59,51 @@ export async function POST(req: Request) {
     const user = {
         clerkId: id,
         email: email_addresses[0].email_address,
-        username: username,
-        firstName: first_name,
-        lastName: last_name,
+        username: username!,
+        firstName: first_name!, 
+        lastName: last_name!,
         photo: image_url
     }
 
-    // const newUser = await createUser(user);
+    const newUser = await createUser(user);
+
+    if(newUser){
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id
+        }
+      })
+    }
+    return NextResponse.json({message: 'OK', user: newUser})
   }
+
+  //UPDATE USER
+  if (eventType === 'user.updated') {
+    const {id, image_url, first_name, last_name, username } = evt.data
+
+    const user = {
+      firstName: first_name!,
+      lastName: last_name!,
+      username: username!,
+      photo: image_url,
+    }
+
+    const updatedUser = await updateUser(id, user)
+
+    return NextResponse.json({ message: 'OK', user: updatedUser })
+  }
+
+  //delete user
+  if (eventType === 'user.deleted') {
+    const { id } = evt.data
+
+    const deletedUser = await deleteUser(id!)
+
+    return NextResponse.json({ message: 'OK', user: deletedUser })
+  }
+ 
+  
+ 
 
 
   return new Response('', { status: 200 })
